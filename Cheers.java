efa0b6c3eac1d695ddae295876f0fd214b06e4a8
lihi shapira <lihi.shapira@post.runi.ac.il>
2025-11-08 22:17:56 +0200

    Completed assignment

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,25 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
 
 // Prints a crowd cheering output.
 public class Cheers {
-        public static void main(String[] args) {
+        public static void main(String[] args)
+         {
 	    // Replace this comment with your code
+            String word = args[0];
+            int n = Integer.parseInt(args[1]);
+            word = word.toUpperCase();
+            String spelling = "AEFHILMNORSX";
+            for (int i = 0; i < word.length(); i++)
+               if (spelling.indexOf(word.charAt(i)) == -1)
+              {
+                        System.out.println("Give me a  " + word.charAt(i) + ": "+word.charAt(i)+"!");
+                }
+                 else   System.out.println("Give me an " + word.charAt(i) + ": "+word.charAt(i)+"!");
+       
+            System.out.println("What does that spell?");
+                for (int j = 0; j < n; j++)
+                {
+                        System.out.println(word + "!!!");
+                }
+
         }
 }
