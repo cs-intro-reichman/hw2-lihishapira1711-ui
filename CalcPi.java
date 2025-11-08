67b85c06e2ce57be28632acb9ee2e65f63f83634
lihi shapira <lihi.shapira@post.runi.ac.il>
2025-11-08 22:33:04 +0200

    Completed assignment

--- CalcPi.java
+++ CalcPi.java
@@ -2,5 +2,14 @@
 public class CalcPi {
 	public static void main(String [] args) { 
 	    // Replace this comment with your code
+			int n = Integer.parseInt(args[0]);
+			double piApproximation = 0.0;
+			for (int k = 0; k < n; k++) {
+				double term = Math.pow(-1, k) / (2 * k + 1);
+				piApproximation += term;
+			}
+			piApproximation *= 4;
+			System.out.println(" pi according to java: " + Math.PI);
+			System.out.println(" pi, approximated:     " + piApproximation);
 	}
 }
